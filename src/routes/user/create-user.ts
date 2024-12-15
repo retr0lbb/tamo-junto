29a604491abe5258fb417ea248c6ce46b9b26de7
retr0lbb <retr0lbb@gmail.com>
2024-12-15 18:22:45 -0300

    feat: add prisma integration with routes

--- src/routes/user/create-user.ts
+++ src/routes/user/create-user.ts
@@ -1,10 +1,12 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { prisma } from "../../lib/prisma";
 
 export async function createUserHandler(
 	request: FastifyRequest,
 	reply: FastifyReply,
 ) {
-	return reply.status(200).send("Hey man");
+	const results = await prisma.user.findMany();
+	return reply.status(200).send({ status: 200, data: results });
 }
 
 export async function createUserRoute(app: FastifyInstance) {
