44ec46264c6794a149497593da323c75c9a6d880
retr0lbb <retr0lbb@gmail.com>
2024-12-17 23:56:04 -0300

    feat: add url query params

--- src/routes/campaing/list-campaings.ts
+++ src/routes/campaing/list-campaings.ts
@@ -1,16 +1,34 @@
 import type { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import { prisma } from "../../lib/prisma";
+import z from "zod";
+
+const queryParams = z.object({
+	page: z
+		.string()
+		.transform((val) => Number.parseInt(val, 10))
+		.optional()
+		.default("0"),
+	user: z.string().uuid().optional().nullable(),
+});
 
 export async function listCampaingsHandler(
-	_request: FastifyRequest,
+	request: FastifyRequest,
 	reply: FastifyReply,
 ) {
+	const { user, page } = queryParams.parse(request.query);
+
+	const resultsPerPage = 10;
 	const campaings = await prisma.campaing.findMany({
 		select: {
 			id: true,
 			name: true,
 			goal: true,
 		},
+		where: {
+			Userid: user ?? undefined,
+		},
+		take: resultsPerPage,
+		skip: page * resultsPerPage,
 	});
 
 	if (campaings.length <= 0) {
