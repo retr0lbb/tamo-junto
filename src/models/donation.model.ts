b69e0144e50514ec369e05492edb5ee86b79e0e8
retr0lbb <retr0lbb@gmail.com>
2024-12-23 21:39:13 -0300

    refactor: add donation model

---
+++ src/models/donation.model.ts
@@ -0,0 +1,91 @@
+import type { PrismaClient as PrismaClientDb } from "@prisma/client";
+import { Prisma as PrismaClient } from "@prisma/client";
+import { NotFound } from "../_errors/notFoundError";
+import { ClientError } from "../_errors/clientError";
+import donationEvent from "../events/emiters/donation.events";
+
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+export class DonationModel {
+	static async donateToCampaing(
+		db: PrismaClientDb,
+		data: { campaingId: string; userId: string; donnationAmmount: number },
+	) {
+		const campaing = await db.campaing.findUnique({
+			where: {
+				id: data.campaingId,
+			},
+		});
+
+		if (!campaing) {
+			throw new NotFound("Campaing not founded");
+		}
+
+		if (campaing.Userid === data.userId) {
+			throw new Error("Cannot donate to your own campaing");
+		}
+
+		const donationsArray = await db.donation.findMany({
+			where: {
+				Campaingid: data.campaingId,
+			},
+			select: {
+				donationAmmount: true,
+			},
+		});
+
+		const totalCollectedValueOfCampaing = donationsArray.reduce(
+			(acc, donation) => {
+				return acc.plus(donation.donationAmmount);
+			},
+			new PrismaClient.Decimal(0),
+		);
+
+		if (
+			totalCollectedValueOfCampaing.comparedTo(campaing.goal) === 1 ||
+			totalCollectedValueOfCampaing.comparedTo(campaing.goal) === 0
+		) {
+			throw new ClientError(
+				"cannot donate to a campaing thats already completed",
+			);
+		}
+
+		if (
+			totalCollectedValueOfCampaing
+				.plus(data.donnationAmmount)
+				.comparedTo(campaing.goal) === 1
+		) {
+			throw new ClientError("Your donation will surpass the campaing goal.");
+		}
+
+		const user = await db.user.findUnique({
+			where: {
+				id: data.userId,
+			},
+			select: { email: true },
+		});
+		if (!user) {
+			throw new ClientError("The user of this token no longer exits");
+		}
+
+		const donation = await db.donation.create({
+			data: {
+				donationAmmount: data.donnationAmmount,
+				Campaingid: data.campaingId,
+				Userid: data.userId,
+			},
+		});
+
+		donationEvent.emitCheckIfMilestoneIsCompleted(
+			campaing.id,
+			totalCollectedValueOfCampaing.toNumber() + data.donnationAmmount,
+		);
+
+		donationEvent.sendEmail({
+			subject: "Donation complete",
+			text: `You sucessfully donated to ${campaing.name} with the value of R$ ${data.donnationAmmount}`,
+			to: user.email,
+		});
+
+		return donation;
+	}
+}
--- src/routes/donations/donate-to-campaing.ts
+++ src/routes/donations/donate-to-campaing.ts
@@ -7,10 +7,7 @@ import { Campaing } from "../../models/campaing.model";
 import donationEvent from "../../events/emiters/donation.events";
 import { ClientError } from "../../_errors/clientError";
 import { ServerError } from "../../_errors/serverError";
-import {
-	generatePaymentIntent,
-	generatePaymentSession,
-} from "../../lib/payment";
+import { DonationModel } from "../../models/donation.model";
 
 export const createDonationSchema = z.object({
 	donationAmmount: z.number().positive().nonnegative().min(0.01),
@@ -29,78 +26,10 @@ export async function createDonationHandler(
 	const { id } = createDonationRouteParams.parse(request.params);
 
 	try {
-		const campaing = await Campaing.verifyIfCampaingExists(prisma, { id });
-
-		if (!campaing) {
-			return reply.status(404).send({ message: "Campaing not found" });
-		}
-
-		if (campaing.Userid === userId) {
-			return reply
-				.status(403)
-				.send({ message: "You cannot donate to your own campaing" });
-		}
-
-		const donationsArray = await prisma.donation.findMany({
-			where: {
-				Campaingid: id,
-			},
-			select: {
-				donationAmmount: true,
-			},
-		});
-
-		const totalCollectedValueOfCampaing = donationsArray.reduce(
-			(acc, donation) => {
-				return acc.plus(donation.donationAmmount);
-			},
-			new PrismaClient.Decimal(0),
-		);
-
-		if (
-			totalCollectedValueOfCampaing
-				.plus(donationAmmount)
-				.comparedTo(campaing.goal) === 1
-		) {
-			return reply
-				.status(400)
-				.send({ message: "Cannot donate to a campaing that achived its goal" });
-		}
-
-		const user = await prisma.user.findUnique({
-			where: {
-				id: userId,
-			},
-			select: { email: true },
-		});
-		if (!user) {
-			throw new ClientError("The user of this token no longer exits");
-		}
-
-		const donation = await prisma.donation.create({
-			data: {
-				donationAmmount,
-				Campaingid: id,
-				Userid: userId,
-			},
-		});
-
-		const result = await generatePaymentSession({
-			campaingName: campaing.name,
-			price: donationAmmount,
-		});
-
-		console.log(result);
-
-		donationEvent.emitCheckIfMilestoneIsCompleted(
-			campaing.id,
-			totalCollectedValueOfCampaing.toNumber() + donationAmmount,
-		);
-
-		donationEvent.sendEmail({
-			subject: "Donation complete",
-			text: `You sucessfully donated to ${campaing.name} with the value of R$ ${donationAmmount}`,
-			to: user.email,
+		const donation = await DonationModel.donateToCampaing(prisma, {
+			campaingId: id,
+			donnationAmmount: donationAmmount,
+			userId: userId,
 		});
 
 		return reply.status(201).send({
@@ -109,7 +38,7 @@ export async function createDonationHandler(
 		});
 	} catch (error) {
 		console.log(error);
-		throw new ServerError("An error occurred at donations");
+		throw error;
 	}
 }
 
